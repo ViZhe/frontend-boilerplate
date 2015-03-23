34fec0a5b25a2263918fdefc4aa25e156fbe8022
ViZhe <vizhe.cat@ya.ru>
2015-03-23 22:37:11 +0300

    component popup add

---
+++ source/components/popup/popup.js
@@ -0,0 +1,55 @@
+var popup = (function() {
+	var body = document.querySelector("body");
+ 	return {
+		init: function(e) {
+			e = e.getAttribute('data-popup');
+			item = document.querySelector(e);
+			itemInner = document.querySelector(e + " .b-popup__inner");
+			itemContent = document.querySelector(e + " .b-popup__content");
+		},
+		scrollWidth: function(act) {
+			if(act == "show"){
+				var b = body.offsetWidth;
+				body.classList.add("b-popup__body");
+				var c = body.offsetWidth;
+				var scrollWidth = c - b + "px";
+				body.style.marginRight = scrollWidth;
+			} else {
+				body.classList.remove("b-popup__body");
+				body.style.marginRight = 0;
+			}
+		},
+		createdAddElem: function(elem, parent) {
+			newItem = document.createElement("div");
+			newItem.className = elem;
+			parent.insertBefore(newItem, parent.firstChild);
+			popup.addEvent(elem);
+		},
+		removeElem: function(elem) {
+			document.querySelector(elem).parentNode.removeChild(document.querySelector(elem));
+		},
+		addEvent: function(elem) {
+			document.querySelector("."+elem).addEventListener("click", function(e) {
+				popup.hide();
+			});
+		},
+		show: function() {
+			popup.init(this);
+			popup.createdAddElem("b-popup__shadow", itemInner);
+			popup.createdAddElem("b-popup__close", itemContent);
+			popup.scrollWidth("show");
+			item.classList.add("b-popup_show");
+		},
+		hide: function() {
+			document.querySelector('.b-popup_show').classList.remove("b-popup_show");
+			popup.scrollWidth();
+			popup.removeElem(".b-popup__shadow");
+			popup.removeElem(".b-popup__close");
+		}
+ 	};
+})();
+
+var popupShowEl = document.querySelectorAll("[data-popup]");
+for(var i = 0; i < popupShowEl.length; i++) {
+	popupShowEl[i].onclick = popup.show;
+}
--- source/js/hoppas.js
+++ source/js/hoppas.js
@@ -1,4 +1,9 @@
 ﻿
+// Vanila JS
+@@include('../components/popup/popup.js')
+
+
+// jQuery
 @@include('base/jquery-2.1.3.js')
 
 
