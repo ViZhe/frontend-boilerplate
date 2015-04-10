89e8767c598a522f27f2695472ae52391ee0ed4a
ViZhe <vizhe.cat@ya.ru>
2015-04-11 01:34:15 +0300

    popup rewrite
    back inner, new script, new logic

--- source/components/popup/popup.js
+++ source/components/popup/popup.js
@@ -1,34 +1,48 @@
 
-$("[data-popup]").on("click", popup);
+var popup = (function () {
+    "use strict";
 
-function popup() {
-	$("[data-popup]").off("click", popup);
-	var body = $("body");
-	var widthInner = body.width();
-	body.addClass("b-popup__body");
-	var widthOuter = body.width();
-	var width = widthOuter - widthInner +"px";
-	body.css("margin-right", width)
+	var body,
+		_this,
+		elHide,
+		items = $("[data-popup]");
 
-	var el = $($(this).data().popup);
-
-	el.prepend('<div class="b-popup__shadow"></div>');
-	el.find(".b-popup__content").prepend('<div class="b-popup__close"></div>');
-
-	if(el.find(".b-popup__content").height() > window.innerHeight){
-		el.addClass("b-popup_big") ;
-		$('.b-popup__shadow').css("margin-right", width)
-	}
-	el.addClass("b-popup_show") ;
-
-	function close() {
-		$(this).off("click", close);
-		$(".b-popup_show").removeClass("b-popup_show");
-		setTimeout(function() {
-			body.removeClass("b-popup__body").removeAttr("style");
-			$(".b-popup__close, .b-popup__shadow").remove();
-			$("[data-popup]").on("click", popup);
-		}, 300)
+	var popup = {
+		init: function () {
+			body = $("body");
+			_this = this;
+			items.each( function () {
+				var el = $($(this).data().popup);
+				var content = el.html();
+				el.html('<div class="b-popup__inner"><div class="b-popup__content"><div class="b-popup__close"></div>' + content + '</div></div>');
+			});
+			elHide = $(".b-popup__inner, .b-popup__close");
+			items.on("click", _this.show);
+		},
+		body: function () {
+			var widthInner = body.width();
+			body.addClass("b-popup__body");
+			var widthOuter = body.width();
+			var width = widthOuter - widthInner +"px";
+			body.css("margin-right", width)
+		},
+		show: function () {
+			_this.body();
+			items.off("click", popup.show);
+			var el = $($(this).data().popup);
+			el.addClass("b-popup_show") ;
+			elHide.on("click", _this.hide);
+		},
+		hide: function (e) {
+			if ( $(e.target).is('.b-popup__inner, .b-popup__close') ) {
+				elHide.off("click", _this.hide);
+				$(".b-popup_show").removeClass("b-popup_show");
+				setTimeout( function() {
+					body.removeClass("b-popup__body").removeAttr("style");
+					items.on("click", popup.show);
+				}, 300)
+			}
+		}
 	};
-	$(".b-popup__shadow, .b-popup__close").on("click", close);
-};
+	popup.init();
+})();
