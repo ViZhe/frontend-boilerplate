39bee2dd4f68deacfd6ca73e46a68462f6fc34c2
ViZhe <vizhe.cat@ya.ru>
2015-04-11 20:09:00 +0300

    gallery structure

---
+++ source/components/gallery/gallery.js
@@ -0,0 +1,33 @@
+
+var gallery = (function () {
+    "use strict";
+
+	var body;
+
+	var gallery = {
+		init: function () {
+		},
+
+        build: function () {
+		},
+
+        addTitle: function () {
+		},
+
+        closeSlide: function () {
+		},
+
+        toSlide: function () {
+		},
+        prevSlide: function () {
+		},
+        nextSlide: function () {
+		},
+
+        destroy: function () {
+		}
+
+
+	};
+    gallery.init();
+})();
