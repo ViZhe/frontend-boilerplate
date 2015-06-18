baafed108831791303ad12ce5d8dd530d72054c7
ViZhe <vizhe.cat@ya.ru>
2015-06-18 23:49:33 +0300

    hoppas.js clear

--- source/js/hoppas.js
+++ source/js/hoppas.js
@@ -1,24 +1,11 @@
 
-/* dom */
-
-@@include('../components/popup/popup.js')
-// @ @include('../components/light-gallery/light-gallery.dom.js')
-// var lightGallery = new lightGallery('.js-light-gallery');
-
-// $(document).ready(function() {
-//
-// });
-
-
-// $("*").on("click", function() {
-//
-// });
-
-
 /* jQuery */
 
-//@ @include('../components/light-gallery/light-gallery.jquery.js')
-//$('.js-light-gallery').lightGallery();
+// @ @include('../components/popup/popup.js')
+
+// @ @include('../components/light-gallery/light-gallery.jquery.js')
+// $('.js-light-gallery').lightGallery();
+
 
 // $(document).ready(function() {
 //
