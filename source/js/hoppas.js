7a01962e6644999015de6b9659575fd9d63378a7
ViZhe <vizhe.cat@ya.ru>
2015-06-21 20:27:36 +0300

    form: js rename, eslint fix tab

--- source/js/hoppas.js
+++ source/js/hoppas.js
@@ -1,8 +1,9 @@
 
 /* jQuery */
 
+@@include('../components/form/form.jquery.js')
+
 // @ @include('../components/popup/popup.js')
-@@include('../components/form/form.js')
 
 // @ @include('../components/light-gallery/light-gallery.jquery.js')
 // $('.js-light-gallery').lightGallery();
