e67491368c044751c960708bc2da0e9bcd96cd55
ViZhe <vizhe.cat@ya.ru>
2016-07-19 20:09:28 +0300

    moved logic includes in self file

---
+++ source/components/light-gallery/light-gallery.js
@@ -0,0 +1,16 @@
+
+/* eslint spaced-comment:0 */
+
+const lg = function () {
+  //= lg.js
+  //= lg-hash.js
+  //= lg-pager.js
+  //= lg-thumbnail.js
+
+  // = lg-video.js
+  // = lg-fullscreen.js
+  // = lg-zoom.js
+  // = lg-autoplay.js
+}
+
+lg()
