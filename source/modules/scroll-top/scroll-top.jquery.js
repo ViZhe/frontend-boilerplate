84f929c6f814a3c63da0619d910bcd0f57551a32
ViZhe <vizhe.cat@ya.ru>
2016-07-19 20:08:14 +0300

    rewrite scroll-top script

--- source/modules/scroll-top/scroll-top.jquery.js
+++ source/modules/scroll-top/scroll-top.jquery.js
@@ -1,19 +1,21 @@
 
-(($, window, document) => {
-  const backToTop = $('.js-scroll-top')
+const hopScrollTop = function () {
+  const selector = $('.js-scroll-top')
 
   $(window).on('scroll', () => {
     const scrollTop = $(document).scrollTop()
     if (scrollTop > 100) {
-      backToTop.addClass('m-scroll-top_show')
+      selector.addClass('m-scroll-top_show')
     } else {
-      backToTop.removeClass('m-scroll-top_show')
+      selector.removeClass('m-scroll-top_show')
     }
   })
 
-  backToTop.on('click', () => {
+  selector.on('click', () => {
     $('body, html').animate({
       scrollTop: 0
     }, 700)
   })
-})(jQuery, window, document)
+}
+
+hopScrollTop()
